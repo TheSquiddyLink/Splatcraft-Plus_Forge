01c6bac426ff2dd9aec63c34a3de8c1614c94f7d
TheSquiddyLink <preston@happychaps.com>
2024-04-24 08:27:20 -0700

    Splattershot class
    Octoshot uses the splatershot class

--- src/main/java/com/salmonpants/splatcraftplus/items/weapons/order_weapons/OctoShot.java
+++ src/main/java/com/salmonpants/splatcraftplus/items/weapons/order_weapons/OctoShot.java
@@ -1,15 +1,9 @@
 package com.salmonpants.splatcraftplus.items.weapons.order_weapons;
-import net.splatcraft.forge.items.weapons.ShooterItem;
-import net.splatcraft.forge.items.weapons.settings.WeaponSettings;
-import net.splatcraft.forge.registries.SplatcraftItems;
-import net.minecraftforge.registries.RegistryObject;
 
-public class OctoShot extends ShooterItem {
-    private static RegistryObject<ShooterItem> splattershot = SplatcraftItems.splattershot;
-    private static ShooterItem item = splattershot.get();
-    private static WeaponSettings settings = item.settings;
-    public OctoShot() {
-        super(settings);
-    }
+import com.salmonpants.splatcraftplus.items.weapons.defaultTypes.Splattershot;
+
+
+public class OctoShot extends Splattershot {
+
 }
 
