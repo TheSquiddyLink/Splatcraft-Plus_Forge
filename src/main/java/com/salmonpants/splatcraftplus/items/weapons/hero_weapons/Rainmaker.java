3f8117f97d5046090112baf92c7d511e2715ad5b
TheSquiddyLink <preston@happychaps.com>
2024-04-24 08:32:56 -0700

    Add Rarity for Octoshot and Rainmaker

--- src/main/java/com/salmonpants/splatcraftplus/items/weapons/hero_weapons/Rainmaker.java
+++ src/main/java/com/salmonpants/splatcraftplus/items/weapons/hero_weapons/Rainmaker.java
@@ -1,5 +1,6 @@
 package com.salmonpants.splatcraftplus.items.weapons.hero_weapons;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Rarity;
 import net.splatcraft.forge.handlers.PlayerPosingHandler;
 import net.splatcraft.forge.items.weapons.SlosherItem;
 
@@ -7,7 +8,10 @@ public class Rainmaker extends SlosherItem {
 
     public Rainmaker() {
         super("rainmaker");
-
+    }
+    @Override
+    public Rarity getRarity(ItemStack stack) {
+        return Rarity.EPIC;
     }
     @Override
     public PlayerPosingHandler.WeaponPose getPose(ItemStack stack)
--- src/main/java/com/salmonpants/splatcraftplus/items/weapons/order_weapons/OctoShot.java
+++ src/main/java/com/salmonpants/splatcraftplus/items/weapons/order_weapons/OctoShot.java
@@ -4,8 +4,15 @@ import static com.salmonpants.splatcraftplus.SplatcraftPlus.WEAPONS;
 
 import com.salmonpants.splatcraftplus.items.weapons.template_weapon.Shooter;
 
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Rarity;
+
 public class OctoShot extends Shooter {
     public OctoShot(){
         super(WEAPONS);
+    }
+    @Override
+    public Rarity getRarity(ItemStack stack) {
+        return Rarity.EPIC;
     }
 }
