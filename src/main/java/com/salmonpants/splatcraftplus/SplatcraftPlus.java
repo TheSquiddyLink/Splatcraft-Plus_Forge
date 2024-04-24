7ca3c081b62ac6cd992ce15b6f8c4a77084e3c80
TheSquiddyLink <preston@happychaps.com>
2024-04-24 08:32:25 -0700

    Added Marina Palette

--- src/main/java/com/salmonpants/splatcraftplus/SplatcraftPlus.java
+++ src/main/java/com/salmonpants/splatcraftplus/SplatcraftPlus.java
@@ -82,6 +82,7 @@ public class SplatcraftPlus
     public static final RegistryObject<EmptyPalette> PEARL_PALETTE = ITEMS.register("pearl_palette",() -> new EmptyPalette());
     public static final RegistryObject<EmptyPalette> AGENT_4_PALETTE = ITEMS.register("agent_4_palette",() -> new EmptyPalette());
     public static final RegistryObject<EmptyPalette> CALLIE_PALETTE = ITEMS.register("callie_palette",() -> new EmptyPalette());
+    public static final RegistryObject<EmptyPalette> MARINA_PALETTE = ITEMS.register("marina_palette",() -> new EmptyPalette());
 
     // Chips
     public static final RegistryObject<Item> POWER_CHIP = ITEMS.register("power_chip", () -> new ColorChip());
