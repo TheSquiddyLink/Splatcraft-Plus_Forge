50cd646b12d8b957b993e2bb2a4e27a8b530b84e
TheSquiddyLink <preston@happychaps.com>
2024-04-24 08:28:37 -0700

    Added Order Brush Item
    Textures and models has not been added

--- src/main/java/com/salmonpants/splatcraftplus/SplatcraftPlus.java
+++ src/main/java/com/salmonpants/splatcraftplus/SplatcraftPlus.java
@@ -49,6 +49,7 @@ public class SplatcraftPlus
     public static final RegistryObject<DualieItem> ORDER_DUALIES = ITEMS.register("order_dualie", () -> new OrderDualie());
     public static final RegistryObject<BlasterItem> ORDER_BLASTER = ITEMS.register("order_blaster", () -> new OrderBlaster());
     public static final RegistryObject<SlosherItem> ORDER_SLOSHER = ITEMS.register("order_slosher", () -> new OrderSlosher());
+    public static final RegistryObject<RollerItem> ORDER_BRUSH = ITEMS.register("order_brush", () -> new OrderBrush());
 
     {
         // Register the setup method for modloading
---
+++ src/main/java/com/salmonpants/splatcraftplus/items/weapons/order_weapons/OrderBrush.java
@@ -0,0 +1,7 @@
+package com.salmonpants.splatcraftplus.items.weapons.order_weapons;
+
+import com.salmonpants.splatcraftplus.items.weapons.defaultTypes.OctoBrush;
+
+public class OrderBrush extends OctoBrush{
+    
+}
