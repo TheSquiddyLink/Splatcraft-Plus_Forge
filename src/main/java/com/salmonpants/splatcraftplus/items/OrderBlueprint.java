6d7600ea4ab561b365864f8bd08d69b541cfd3ab
TheSquiddyLink <preston@happychaps.com>
2024-04-24 08:27:50 -0700

    Pool remove if
    The pool will now remove the advancement if the user already has it

--- src/main/java/com/salmonpants/splatcraftplus/items/OrderBlueprint.java
+++ src/main/java/com/salmonpants/splatcraftplus/items/OrderBlueprint.java
@@ -6,6 +6,8 @@ import net.minecraft.world.item.Item;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
+
+import net.minecraft.advancements.Advancement;
 import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
@@ -21,26 +23,25 @@ public class OrderBlueprint extends Item {
         "ordershot",
         "order_dualie"
     };
-    private static ArrayList<String> pool = new ArrayList<String>(Arrays.asList(ADVANCMENTS));
-
     public OrderBlueprint() {
         super(new Item.Properties().tab(CreativeModeTab.TAB_MISC));
     }
     @Override
     public InteractionResultHolder<ItemStack> use(Level world, Player player, InteractionHand hand) {
         if (player.isShiftKeyDown() && player instanceof ServerPlayer) {
-            // Check if the player is sneaking and is a server player
+            ArrayList<Advancement> pool = new ArrayList<Advancement>();
             ServerPlayer serverPlayer = (ServerPlayer) player;
+            for (String advancement : ADVANCMENTS) {
+                pool.add(serverPlayer.getServer().getAdvancements().getAdvancement(new ResourceLocation(PATH+advancement)));
+            }
+            pool.removeIf(advancement -> serverPlayer.getAdvancements().getOrStartProgress(advancement).isDone());
+            // Check if the player is sneaking and is a server player
             // Retrieve the advancement for the player
             Random random = new Random();
-            int randomIndex = random.nextInt(pool.size());
-            ResourceLocation advancementId = new ResourceLocation(PATH+pool.get(randomIndex));
-            if (serverPlayer.getServer().getAdvancements().getAdvancement(advancementId) != null) {
-                // Grant the advancement to the player
-                serverPlayer.getAdvancements().award(serverPlayer.getServer().getAdvancements().getAdvancement(advancementId), "default");
-                // Display a message to the player
-                serverPlayer.displayClientMessage(new TranslatableComponent(PATH+pool.get(randomIndex)), true);
-            }
+            if(pool.size() > 0){
+                int randomIndex = random.nextInt(pool.size());
+                serverPlayer.getAdvancements().award(pool.get(randomIndex), "default");
+            } else serverPlayer.displayClientMessage(new TranslatableComponent("No new unlocks"), true);
         }
         // Return PASS to allow the default behavior (e.g., placing a block if applicable)
         return InteractionResultHolder.pass(player.getItemInHand(hand));
